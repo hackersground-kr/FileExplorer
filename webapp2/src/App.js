f51b0c9810b9a4dc7045f34577bd528ab62e7285
chanbin <chanbin5634@gmail.com>
2023-06-22 04:29:40 +0900

    main_app

--- webapp2/src/App.js
+++ webapp2/src/App.js
@@ -12,24 +12,37 @@ import  Main from './Main';
 
 
 function App() {
-  
-  return (
-    <div>
+  useEffect(()=>{
     
-        <Header/>
-       
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={ <Main/>} />
-                    <Route path='/DaguePeople' element={<DaguePeople/>}/>
-                    <Route path='/DaeguCityhall' element={<DaeguCityhall/>}/>
-                    <Route path='/DaeguNews' element={<DaeguNews/>}/>
-                    <Route path='/DaguePeople' element={<DaguePeople/>}/>
-                    <Route path='/Login_signup' element={<Login_signup/>}/>
-                </Routes>
-            </BrowserRouter>
-       
-        <Footer/>
+    setTimeout(()=>{
+        
+        document.getElementById('wrap').style.opacity = 1;
+        document.getElementById('first').style.opacity = 0;
+    },2000);
+  },[])
+
+  return (
+    <div id='App' >
+         <div id='first'>
+            <h1>대구 취업 서비스</h1>
+         </div>
+
+          <div id='wrap'>
+              <Header/>
+            
+                  <BrowserRouter>
+                      <Routes>
+                          <Route path='/' element={ <Main/>} />
+                          <Route path='/DaguePeople' element={<DaguePeople/>}/>
+                          <Route path='/DaeguCityhall' element={<DaeguCityhall/>}/>
+                          <Route path='/DaeguNews' element={<DaeguNews/>}/>
+                          <Route path='/DaguePeople' element={<DaguePeople/>}/>
+                          <Route path='/Login_signup' element={<Login_signup/>}/>
+                      </Routes>
+                  </BrowserRouter>
+            
+              <Footer/>
+          </div>
     </div>
   );
 }
--- webapp2/src/Footer.js
+++ webapp2/src/Footer.js
@@ -3,7 +3,35 @@ import './css/Footer.css';
 export default function Footer(){
   return(
     <footer>
+        <div>
+          <img src="static/img/cityhall.png" /> 
+          <p>대구시청</p>
+        </div>
         
+        <div>
+            
+          <img src='static/img/job.png'/> 
+          <p>일자리</p>
+        </div>
+
+        <div>
+            
+          <img src='static/img/home.png'/> 
+          <p>홈</p>
+        </div>
+
+        <div>
+            
+          <img src='static/img/news.png'/> 
+          <p>뉴스</p>
+        </div>
+
+        <div>
+            
+          <img src='static/img/People.png'/> 
+          <p>시민혜택</p>
+        </div>
+
     </footer>
   )
 }
