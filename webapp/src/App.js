1f708a0b5e3fc476389e5a1be1ed482c8ed1356b
chanbin <chanbin5634@gmail.com>
2023-06-22 02:01:03 +0900

    app

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,60 +3,33 @@ import './App.css';
 import  { React,useEffect, useRef } from 'react';
 
 function App() {
-  // const deferredPrompt = useRef(null);
-
-  // const showInstallButton = () => {
-  //   const installButton = document.getElementById('install-button');
-  //   installButton.style.display = 'block';
-  // };
-
-  // const handleInstall = () => {
-  //   alert('ss');  
-  //   if (deferredPrompt.current) {
-  //     deferredPrompt.current.prompt();
-  //     deferredPrompt.current.userChoice.then((choiceResult) => {
-  //       if (choiceResult.outcome === 'accepted') {
-  //         console.log('앱이 설치되었습니다.');
-  //       } else {
-  //         console.log('앱 설치가 거부되었습니다.');
-  //       }
-  //       deferredPrompt.current = null;
-  //     });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //     const handleBeforeInstallPrompt = (e) => {
-  //       e.preventDefault();
-  //       deferredPrompt.current = e;
-  //       showInstallButton();
-  //     };
   
-  //     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-  
-  //     return () => {
-  //       window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-  //     };
-
-  // }, []);
-
   return (
     <div>
-      {/* 앱의 내용 */}
+    
   
       <header>
-            <nav>
-            <button id="install-button" >
-            앱 설치
-          </button>
-              <a href=''>시청공고</a>
-              <a href=''>채용공고</a>
-              <a href=''>대구뉴스</a>
-              <a href=''>시민혜택</a>
-              <a href='' >로그인</a>
-              <a href='' >회원가입</a>
-            </nav>
+              <h1>대구일자리</h1>
+              <label htmlFor="check">
+                <img src="static/img/bar.png" />
+              </label>
+              <input type='checkbox' id='check'></input>
+              <nav>   
+                  <label htmlFor="check">
+                    <img id='close' src="static/img/close.png" />
+                  </label>
+                  <a href=''>시청공고</a>
+                  <a href=''>채용공고</a>
+                  <a href=''>대구뉴스</a>
+                  <a href=''>시민혜택</a>
+                  <a href='' >로그인</a>
+                  <a href='' >회원가입</a>
+                </nav>
         </header>
+
+        <main>
+  
+        </main>
     </div>
   );
 }
