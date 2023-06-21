b4a758a206cfcf3d2cab0d53b443d31befc58369
cumedang <yusun2234sun@gmail.com>
2023-06-22 00:03:52 +0900

    세션값 추가

--- main.go
+++ main.go
@@ -5,16 +5,20 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/cumedang/FileExplorer/backend/loginup"
 	"github.com/cumedang/FileExplorer/backend/sign"
 	"github.com/cumedang/FileExplorer/backend/utils"
+	"github.com/gorilla/sessions"
 )
 
 var port = ":4000"
 
+var store = sessions.NewCookieStore([]byte(os.Getenv("doyouknowsanz")))
+
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("front/signUp/signUp.html")
+	tmpl, err := template.ParseFiles("front/index.html")
 	utils.HandelERror(err)
 	tmpl.Execute(rw, nil)
 }
@@ -33,16 +37,51 @@ func signup(rw http.ResponseWriter, r *http.Request) {
 func login(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
+		session, _ := store.Get(r, "wasanz")
 		r.ParseForm()
 		id := r.Form.Get("userlogin")
 		pw := r.Form.Get("loginpw")
 		fmt.Println(id + " " + pw)
 		answer := loginup.Login(id, pw)
 		if answer == 1 {
-			fmt.Println(1)
+			session.Values["id"] = id
+			session.Values["answer"] = 1
+			err := session.Save(r, rw)
+			utils.HandelERror(err)
+
+			// 세션 값을 JavaScript로 전달하는 쿠키 설정
+			cookieID := &http.Cookie{
+				Name:  "id",
+				Value: id,
+			}
+			http.SetCookie(rw, cookieID)
+
+			cookieAnswer := &http.Cookie{
+				Name:  "answer",
+				Value: "1",
+			}
+			http.SetCookie(rw, cookieAnswer)
+
 			fmt.Fprint(rw, `<script>alert("`+id+`님 환영합니다");location.href='front/index.html'</script>`)
 		} else {
-			fmt.Println(2)
+			session.Values["id"] = nil
+			session.Values["answer"] = 0
+			err := session.Save(r, rw)
+			utils.HandelERror(err)
+
+			// 세션 값을 JavaScript로 전달하는 쿠키 설정
+			cookieID := &http.Cookie{
+				Name:  "id",
+				Value: "",
+			}
+			http.SetCookie(rw, cookieID)
+
+			cookieAnswer := &http.Cookie{
+				Name:  "answer",
+				Value: "0",
+			}
+			http.SetCookie(rw, cookieAnswer)
+
 			fmt.Fprint(rw, `<script>alert('계정이 존재하지 않습니다. 회원가입 먼저 하여주세요'); location.href='front/signup/signup.html'</script>`)
 		}
 	}
