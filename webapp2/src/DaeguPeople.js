f7d13cbcb16ce13696e78d2c75ed5eebd603fab9
pdg0612 <pdg0612tow@gmail.com>
2023-06-22 11:14:31 +0900

    혜태

--- webapp2/src/DaeguPeople.js
+++ webapp2/src/DaeguPeople.js
@@ -1,7 +1,52 @@
-export default function DaguePeople(){
-  return(
+import "./css/DaguePeople.css";
+
+export default function DaguePeople() {
+  return (
     <div>
-      
+      <table>
+        <tr>
+          <th>저소득층</th>
+          <th>고령자</th>
+          <th>장애인</th>
+          <th>청년 정책</th>
+        </tr>
+        <tr>
+          <td>국민 기초 생활 보장</td>
+          <td>어르신정책사업</td>
+          <td>장애인 지원 제도</td>
+          <td>교육기</td>
+        </tr>
+        <tr>
+          <td>시민 행복 보장 제도</td>
+          <td>어르신 복지 시설</td>
+          <td>등록 절차 안내</td>
+          <td>사회 진입기</td>
+        </tr>
+        <tr>
+          <td>긴급 복지 지원</td>
+          <td>장묘 시설 현황</td>
+          <td>장애인 사업 안내</td>
+          <td>정착기</td>
+        </tr>
+        <tr>
+          <td>자활 지원 사업</td>
+          <td></td>
+          <td>장애인 복지 시설</td>
+          <td>공통 사항</td>
+        </tr>
+        <tr>
+          <td>사회 복지 시설</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>바우처 사업 안내</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
     </div>
-  )
+  );
 }
