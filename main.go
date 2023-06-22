e8965dbb5e601d017941be1740ccf14d5f534fc4
cumedang <yusun2234sun@gmail.com>
2023-06-23 03:05:18 +0900

    리엑트 됬나?

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	indexHTML, err := ioutil.ReadFile("/home/site/wwwroot/webapp2/build/index.html")
+	indexHTML, err := ioutil.ReadFile(" /home/site/wwwroot/webapp2/static/index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,24 +22,23 @@ func main() {
 	if port == "" {
 		port = "8080" // 기본 포트 설정
 	}
+
 	mux := http.NewServeMux()
 
-	fs := http.FileServer(http.Dir("static"))
-
 	// js 폴더에 대한 핸들러
-	jsHandler := http.StripPrefix("/static/js/", fs)
+	jsHandler := http.StripPrefix("/static/js/", http.FileServer(http.Dir("static/js")))
 	mux.Handle("/static/js/", jsHandler)
 
 	// css 폴더에 대한 핸들러
-	cssHandler := http.StripPrefix("/static/css/", fs)
+	cssHandler := http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css")))
 	mux.Handle("/static/css/", cssHandler)
 
 	// img 폴더에 대한 핸들러
-	imgHandler := http.StripPrefix("/static/img/", fs)
+	imgHandler := http.StripPrefix("/static/img/", http.FileServer(http.Dir("static/img")))
 	mux.Handle("/static/img/", imgHandler)
 
 	mux.HandleFunc("/", home)
 
-	fmt.Printf("http://localhost:%s", port)
+	fmt.Printf("http://localhost:%s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
