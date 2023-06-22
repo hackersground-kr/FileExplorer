8858892e1b0c55095c5dfc9241b8e50cef02c5b5
cumedang <yusun2234sun@gmail.com>
2023-06-23 04:11:36 +0900

    체인지

--- main.go
+++ main.go
@@ -17,25 +17,26 @@ func home(rw http.ResponseWriter, r *http.Request) {
 	rw.Write(indexHTML)
 }
 
+func serveStaticFiles(dir string, prefix string) http.Handler {
+	fs := http.FileServer(http.Dir(dir))
+	return http.StripPrefix(prefix, fs)
+}
+
 func main() {
 	port := os.Getenv("HTTP_PLATFORM_PORT")
 	if port == "" {
 		port = "8080" // 기본 포트 설정
 	}
-
 	mux := http.NewServeMux()
 
-	// js 폴더에 대한 핸들러
-	jsHandler := http.StripPrefix("/static/js/", http.FileServer(http.Dir("/home/site/wwwroot/webapp2/build/static/js")))
-	mux.Handle("/static/js/", jsHandler)
+	// JS 폴더에 대한 핸들러
+	mux.Handle("/static/js/", serveStaticFiles("/home/site/wwwroot/webapp2/build/static/js/", "/static/js/"))
 
-	// css 폴더에 대한 핸들러
-	cssHandler := http.StripPrefix("/static/css/", http.FileServer(http.Dir("/home/site/wwwroot/webapp2/build/static/css")))
-	mux.Handle("/static/css/", cssHandler)
+	// CSS 폴더에 대한 핸들러
+	mux.Handle("/static/css/", serveStaticFiles("/home/site/wwwroot/webapp2/build/static/css/", "/static/css/"))
 
-	// img 폴더에 대한 핸들러
-	imgHandler := http.StripPrefix("/static/img/", http.FileServer(http.Dir("/home/site/wwwroot/webapp2/build/static/img")))
-	mux.Handle("/static/img/", imgHandler)
+	// Img 폴더에 대한 핸들러
+	mux.Handle("/static/img/", serveStaticFiles("/home/site/wwwroot/webapp2/build/static/img/", "/static/img/"))
 
 	mux.HandleFunc("/", home)
 
